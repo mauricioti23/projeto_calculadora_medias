69237f59c9ae9ef3a2a6302d19efe6ff655071bc
Maurício Souza <147617053+mauricioti23@users.noreply.github.com>
2024-04-17 11:53:59 -0300

    Update main.js
    adicionado limite de 2 casas decimais

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-valor').innerHTML = mediaFina.toFixed(2);
     document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
 }
 
